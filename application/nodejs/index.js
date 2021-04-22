1eaf3d5955d21af75fd2502ed020b79b1ba71bd9
ningeg <30407534+ningeg@users.noreply.github.com>
2021-04-22 13:28:43 +0700

    00

---
+++ application/nodejs/index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:27017";
+console.log(url);
+
+var app = express();
+app.get("/", (req, resp) => {
+	MongoClient.connect( url, (err, client) => {
+			if(err) throw err;
+			console.log("Database connected!");
+
+			const db = client.db("shoppers");
+			db.collection("products").find().toArray((err, result) => {
+					if(err) throw err;
+                    resp.send("000");
+				});
+		}
+	);
+});
+
+app.listen(3000, () => {
+	console.log("Server listening on port 8000");
+});
